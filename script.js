f714414f63ee6fc3b1742549480579dc133d16bb
Igor Gabriel <igorgabrielssrr@gmail.com>
2025-05-01 01:22:54 -0300

    update sobre

--- script.js
+++
@@ -1,4 +0,0 @@
-// Código para abrir e fechar o menu ao clicar no botão
-document.getElementById("menu-btn").addEventListener("click", function() {
-    document.querySelector("nav").classList.toggle("active");
-});
