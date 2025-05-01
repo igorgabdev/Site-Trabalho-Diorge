e8dafe17a263fae6286b82886158589fc75cdfe2
Igor Gabriel <igorgabrielssrr@gmail.com>
2025-05-01 01:20:00 -0300

    Aba Sobre completa

---
+++ script.js
@@ -0,0 +1,4 @@
+// Código para abrir e fechar o menu ao clicar no botão
+document.getElementById("menu-btn").addEventListener("click", function() {
+    document.querySelector("nav").classList.toggle("active");
+});
